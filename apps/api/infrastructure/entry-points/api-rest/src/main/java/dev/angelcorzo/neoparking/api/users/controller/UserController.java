b642d6815b721596396b74d50e52ee6494fcaa60
JuniorCorzo <josedanielmmf@gmail.com>
2025-11-03 22:47:40 -0500

    feat(entry-point): secure user management endpoints

--- apps/api/infrastructure/entry-points/api-rest/src/main/java/dev/angelcorzo/neoparking/api/users/controller/UserController.java
+++ apps/api/infrastructure/entry-points/api-rest/src/main/java/dev/angelcorzo/neoparking/api/users/controller/UserController.java
@@ -19,6 +19,7 @@ import dev.angelcorzo.neoparking.usecase.modifyuserrole.ModifyUserRoleUseCase;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -34,6 +35,7 @@ public class UserController {
   private final DeactivateUserUseCase deactivateUserUseCase;
 
   @PostMapping("/invite-user")
+  @PreAuthorize("hasRole('MANAGER')")
   @ResponseStatus(HttpStatus.CREATED)
   Response<UserInvitationsDTO> inviteUserWithRol(@RequestBody InviteUserDTO inviteUser) {
     final UserInvitations invitationRegistered =
@@ -61,14 +63,17 @@ public class UserController {
   }
 
   @PatchMapping("/modify-rol")
+  @PreAuthorize("hasRole('MANAGER')")
   Response<UserDTO> modifyRol(@RequestBody ModifyRolDTO modifyRol) {
     Users userWithRolUpdate =
         this.modifyUserRoleUseCase.modifyRole(this.userMapper.toModel(modifyRol));
 
-    return Response.ok(this.userMapper.toDTO(userWithRolUpdate), UserMessages.USER_ROL_MODIFIED.toString());
+    return Response.ok(
+        this.userMapper.toDTO(userWithRolUpdate), UserMessages.USER_ROL_MODIFIED.toString());
   }
 
   @DeleteMapping("/deactivate-user")
+  @PreAuthorize("hasRole('MANAGER')")
   Response<Void> deactivateUser(@RequestBody DeactivateUserDTO deactivateUser) {
     this.deactivateUserUseCase.deactivate(this.userMapper.toModel(deactivateUser));
     return Response.ok(null, UserMessages.USER_DEACTIVATED.toString());
